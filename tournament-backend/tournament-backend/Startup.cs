0ffb5a3ea38435c1f9ab02212ef5d13924816ba5
Brad Whitesell <whitesellbt@gmail.com>
2020-06-20 18:07:00 -0700

    feat: added swagger docs and fixed frontend linting issues

--- tournament-backend/tournament-backend/Startup.cs
+++ tournament-backend/tournament-backend/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace tournament_backend
 {
@@ -26,6 +27,22 @@ namespace tournament_backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Nerdneck Tournament API",
+                    Description = "A simple ASP.NET Core Web API for serving data to the tournament frontend pages.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "EchoFrost",
+                        Url = new Uri("https://twitter.com/WC_EchoFrost"),
+                    },
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +52,16 @@ namespace tournament_backend
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nerdneck Tournament API V1");
+            });
 
             app.UseHttpsRedirection();
 

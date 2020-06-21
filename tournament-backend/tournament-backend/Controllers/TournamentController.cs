beb4c6ef8e32d906b7c5b8e0f5c82d86c8b68077
Brad Whitesell <whitesellbt@gmail.com>
2020-06-20 17:57:39 -0700

    feat: added dotnet backend api for serving data to the tournament pages

---
+++ tournament-backend/tournament-backend/Controllers/TournamentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace tournament_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TournamentController : ControllerBase
+    {
+        private readonly ILogger<TournamentController> _logger;
+
+        public TournamentController(ILogger<TournamentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Nerdneck is awesome!";
+        }
+    }
+}
